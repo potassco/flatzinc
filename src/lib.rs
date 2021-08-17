da847146ff2ef99e51ed12f23219465e4fe56b61
Mikael Zayenz Lagerkvist <research@zayenz.se>
2021-08-17 14:32:36 +0200

    Re-export types from top-level
    This commit adds re-exports of relevant types and the top-level
    statement parser combinator. This makes the restructuring of the code
    into modules usable as a drop-in replacement for the flat variant. In
    particular, fzn2lp compiles without changes.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 pub use nom::{
-    error::{convert_error, ErrorKind, FromExternalError, ParseError},
+    error::{convert_error, ErrorKind, FromExternalError, ParseError, VerboseError},
     Err, IResult,
 };
 
@@ -7,3 +7,20 @@ pub mod comments;
 pub mod expressions;
 pub mod primitive_literals;
 pub mod statements;
+
+pub use expressions::{
+    AnnExpr, Annotation, ArrayOfBoolExpr, ArrayOfFloatExpr, ArrayOfIntExpr, ArrayOfSetExpr,
+    BoolExpr, Expr, FloatExpr, IntExpr, SetExpr, SetLiteral, SetLiteralExpr,
+};
+pub use statements::basic_types::BasicType;
+pub use statements::constraints::ConstraintItem;
+pub use statements::parameter_declarations::ParDeclItem;
+pub use statements::parameter_types::{BasicParType, ParType};
+pub use statements::predicate_declarations::PredicateItem;
+pub use statements::predicate_types::{BasicPredParType, PredIndexSet, PredParType};
+pub use statements::solve_items::{Goal, OptimizationType, SolveItem};
+pub use statements::variable_declarations::VarDeclItem;
+pub use statements::variable_types::{BasicVarType, VarType};
+pub use statements::{IndexSet, Stmt};
+
+pub use statements::statement;
