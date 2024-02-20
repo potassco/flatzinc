fe8e3c735b8cd039aa19fa6e4777f99f9b3df774
Sven Thiele <sthiele78@gmail.com>
2024-02-20 19:01:50 +0100

    Return parser

--- examples/fz-parser.rs
+++ examples/fz-parser.rs
@@ -1,12 +1,12 @@
 use anyhow::Result;
-use clap::Parser;
+use clap::Parser as clapParser;
 use log::error;
 use std::path::PathBuf;
 use stderrlog;
-use winnow::error::ContextError;
+use winnow::{error::ContextError, Parser};
 
 /// flatzinc parser
-#[derive(Parser, Debug)]
+#[derive(clapParser, Debug)]
 #[clap(name = "fz-parser")]
 struct Opt {
     /// Input in flatzinc format
@@ -29,11 +29,12 @@ fn run() -> Result<()> {
 
     let opt = Opt::parse();
     let buf = std::fs::read_to_string(opt.file)?;
-    for mut line in buf.lines() {
-        match flatzinc::statement::<ContextError<&str>>(&mut line) {
+    let mut parser = flatzinc::statement::<ContextError<&str>, &str>();
+    for line in buf.lines() {
+        match parser.parse(line) {
             Ok(result) => println!("{:#?}", result),
             Err(e) => {
-                error!("Failed to parse flatzinc!\n{}", e)
+                error!("Failed to parse flatzinc!\n{:?}", e)
             }
         }
     }
