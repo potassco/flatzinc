b45e5c6096d90fddb4f23e75e5d5f04567d30af2
Sven Thiele <sthiele78@gmail.com>
2024-04-12 10:47:13 +0200

    Fix fuzz tests

--- fuzz/fuzz_targets/fuzz_target_1.rs
+++ fuzz/fuzz_targets/fuzz_target_1.rs
@@ -1,14 +1,14 @@
 #![no_main]
 extern crate winnow;
+use flatzinc::Stmt;
 use libfuzzer_sys::fuzz_target;
-pub use winnow::error::ContextError;
 
 fuzz_target!(|data: &[u8]| {
     // fuzzed code goes here
-    if let Ok(mut utf8_str) = std::str::from_utf8(data) {
-        match flatzinc::statement::<ContextError>(&mut utf8_str) {
-            Ok(result) => println!("{:#?}", result),
-            Err(e) => print!("Failed to parse flatzinc: {:?}", e),
+    if let Ok(utf8_str) = std::str::from_utf8(data) {
+        match <Stmt as std::str::FromStr>::from_str(utf8_str) {
+            Ok(result) => println!("{}\n{:#?}", utf8_str, result),
+            Err(_) => print!(" Failed to parse test data."),
         }
     } else {
         // print!("No valid utf8!")
