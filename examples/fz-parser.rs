ae468c360692fe926236c1d73eaad207d081210f
Sven Thiele <sthiele78@gmail.com>
2024-04-12 10:50:41 +0200

    Remove unnecessary mut from example

--- examples/fz-parser.rs
+++ examples/fz-parser.rs
@@ -3,7 +3,6 @@ use clap::Parser;
 use flatzinc::Stmt;
 use log::error;
 use std::path::PathBuf;
-use stderrlog;
 
 /// flatzinc parser
 #[derive(Parser, Debug)]
@@ -29,8 +28,8 @@ fn run() -> Result<()> {
 
     let opt = Opt::parse();
     let buf = std::fs::read_to_string(opt.file)?;
-    for mut line in buf.lines() {
-        match <Stmt as std::str::FromStr>::from_str(&mut line) {
+    for line in buf.lines() {
+        match <Stmt as std::str::FromStr>::from_str(line) {
             Ok(result) => println!("{:#?}", result),
             Err(e) => {
                 error!("Failed to parse flatzinc statement:\n{}", e);
